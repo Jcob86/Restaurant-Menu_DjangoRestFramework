3eaa5086ba2f4f839e93d6ff9c21ff7bddc18923
Jakub Żmudziński <kubaz8645@gmail.com>
2023-02-01 23:52:14 +0100

    design of data structure

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Cart(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    added = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title'] 
+
+
+class Dish(models.Model):
+    title = models.CharField(max_length=255)
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT, related_name='dishes')
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
+    preparation_time = models.DurationField()
+    added = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    is_vegetarian = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.title} for Menu: {self.cart}'
+
+    class Meta:
+        ordering = ['title'] 
---
+++ menu/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+from . import views
+
+router = SimpleRouter()
+
+urlpatterns = router.urls
