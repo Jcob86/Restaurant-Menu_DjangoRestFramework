c0ac5bf07f3b49ce313dc6c9763d914a8ad477b7
Jakub Żmudziński <kubaz8645@gmail.com>
2023-02-03 17:53:40 +0100

    configuration of serializers, views and routing for non public API

--- menu/urls.py
+++ menu/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
 
 router = SimpleRouter()
+router.register('carts', views.CartViewSet)
+router.register('dishes', views.DishViewSet)
 
-urlpatterns = router.urls
-urlpatterns += [path('test', views.test)]
+dishes_router = routers.NestedDefaultRouter(router, 'dishes', lookup='dish')
+dishes_router.register('images', views.DishImageViewSet, basename='dish-images')
+
+
+urlpatterns = router.urls + dishes_router.urls
--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models.aggregates import Count
+from menu.permissions import IsAdminOrReadOnly
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework import status
+from .models import Cart, Dish, DishImage
+from .serializers import CartSerializer, DishImageSerializer, DishSerializer
 
 
-def test(request):
-    return Response('ok')
+class CartViewSet(ModelViewSet):
+    queryset = Cart.objects.annotate(dishes_count=Count('dishes')).all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAdminUser]
+
+    def destroy(self, request, *args, **kwargs):
+        if Dish.objects.filter(cart_id=kwargs['pk']):
+            return Response({'error': 'Cart cannot be deleted because it has dishes'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
+
+
+class DishViewSet(ModelViewSet):
+    queryset = Dish.objects.prefetch_related('images').all()
+    serializer_class = DishSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+class DishImageViewSet(ModelViewSet):
+    serializer_class = DishImageSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_serializer_context(self):
+        return {'dish_id': self.kwargs['dish_pk']}
+
+    def get_queryset(self):
+        return DishImage.objects.filter(dish_id=self.kwargs['dish_pk'])
